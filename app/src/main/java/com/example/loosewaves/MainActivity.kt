27b3c724300dfb9cb6e4d3251c4c537caed2d091
FlorianLUSSON <florianlusson@gmail.com>
2023-09-04 14:33:43 +0200

    push de debut de branch

--- app/src/main/java/com/example/loosewaves/MainActivity.kt
+++ app/src/main/java/com/example/loosewaves/MainActivity.kt
@@ -3,11 +3,8 @@ package com.example.loosewaves
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -16,8 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   GreetingText(message = "Happy birthday Meghan!", from="From Kévin")
+                   GreetingText(message = "Happy birthday Meghan!", from="From Kevin")
                 }
             }
         }
@@ -46,15 +41,12 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier.padding(8.dp))
-
- {
-
+    {
         Text(
             text = message,
             fontSize = 98.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center
-
         )
         Text(
             text = from,
@@ -63,10 +55,11 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LooseWavesTheme {
-        GreetingText("Happy birthday Meghan!", from="From Kévin")
+        GreetingText("Happy birthday Meghan!", from="From Kevin")
     }
 }
