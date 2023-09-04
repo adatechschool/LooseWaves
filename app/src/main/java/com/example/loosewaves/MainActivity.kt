049e6d1b3c6cd544dac18f45a28fb7aa0122a94f
FlorianLUSSON <florianlusson@gmail.com>
2023-09-04 16:35:27 +0200

    Img background + titre centrer

--- app/src/main/java/com/example/loosewaves/MainActivity.kt
+++ app/src/main/java/com/example/loosewaves/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.loosewaves
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,6 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   GreetingText(message = "Happy birthday Meghan!", from="From Kevin")
+                   Page1()
                 }
             }
         }
@@ -37,29 +46,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(8.dp))
-    {
-        Text(
-            text = message,
-            fontSize = 98.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
-        )
-    }
+fun Page1(modifier: Modifier = Modifier) {
+    val imageModifier = Modifier
+        .fillMaxWidth()
+        .padding(0.dp)
+    Image(
+        painter = painterResource(id = R.drawable.img_page_2),
+        contentDescription = "image description",
+        contentScale = ContentScale.FillBounds,
+        modifier = imageModifier
+    )
+    Text(
+        text = "LooseWaves",
+        style = TextStyle(
+            fontSize = 55.sp,
+            fontWeight = FontWeight(500),
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center,
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+//            .background(Color.White)
+    )
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     LooseWavesTheme {
-        GreetingText("Happy birthday Meghan!", from="From Kevin")
+        Page1()
     }
 }
